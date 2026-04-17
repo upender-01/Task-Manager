a9d23c3036240bde2589982f948afbd742a92cce
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-04-18 02:07:24 +0530

    frontend - backend connection f

--- frontend/myapp/src/Components/Home.jsx
+++ frontend/myapp/src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const API_URL = 'https://task-manager-00-9ihe.onrender.com/';
+const API_URL = 'https://task-manager-00-9ihe.onrender.com';
 
 export default function App() {
   const [tasks, setTasks] = useState([]);
