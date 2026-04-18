1d3cac8f623637dcf67f3ccb841bafb91f6be973
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-04-18 11:53:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // In-memory database
 let tasks = [];
-app.get("/" , (req, res){
+app.get("/" , (req, res)=>{
     res.send("Backend is running");
 });
 // GET /tasks - to return all tasks 
